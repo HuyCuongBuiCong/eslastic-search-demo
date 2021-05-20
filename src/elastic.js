930f6c96818b1d438bcbaec12b0ddafb39baf37f
tindinhct93 <tinhdinhct93@gmail.com>
2021-05-20 23:18:49 +0700

    test

--- src/elastic.js
+++ src/elastic.js
@@ -2,6 +2,11 @@ function initEs() {
   $('#es-search').on('keyup',function(){
     console.log('trigger search hear')
   })
+
+  $('#es-search').on('change',function(){
+	this.value = "API content";
+    console.log('Call API OK')
+  })
 }
 
 export {
