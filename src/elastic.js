f6b1fb6e620eec7f26e060c2ceea7e2488bafdbd
tindinhct93 <tinhdinhct93@gmail.com>
2021-05-25 20:08:36 +0700

    CORS eror

--- src/elastic.js
+++ src/elastic.js
@@ -1,12 +1,42 @@
 function initEs() {
+  let link = "https://development.es.ap-southeast-1.aws.found.io:9243/product_sg_dev/_search?pretty=true";
+  let authorization = "Authorization:ApiKey YtMn0dk1SBqt_bmXUwxeOg"
   $('#es-search').on('keyup',function(){
     console.log('trigger search hear')
   })
 
-  $('#es-search').on('change',function(){
-	this.value = "API content";
-    console.log('Call API OK')
-  })
+  $('#es-search').on('change',function () {
+    $.ajax({
+      method: "POST",
+      url: link,
+      dataType: "json",
+      crossDomain: true,
+      //type: 'json',
+      headers: {
+        "Authorization": "Basic XXXXXXXXX",
+        "Access-Control-Allow-Headers":link
+      },
+      data: {
+        "query": {
+          "match": {
+            "title": {"query":"dog dry fuud",
+              "fuzziness":2 }
+          }
+        },
+        "aggs": {
+          "categories.price": {
+            "terms": {
+              "field": "categories.price"
+            }
+          }
+        }
+      },
+      success: function(response) {
+        alert("success");
+        $('#new').html(response);
+      }
+    })}
+  )
 }
 
 export {
